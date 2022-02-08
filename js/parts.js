e6a9c72a5b7eeabbc4824a27aacb19690e3ecb5c
atto-github <59530901+atto-github@users.noreply.github.com>
2022-02-08 23:47:01 +0900

    0208ページ構築

--- js/parts.js
+++ js/parts.js
@@ -20,4 +20,12 @@ $(function() {
 });
 
 
-
+$(function() {
+  new Splide( '.splide', {
+    type : 'loop',
+    height: '20rem', // スライダーの高さ @type: { number|string }, default: ０
+    gap: '1em', // スライド間のギャップサイズ @type: { number|string }, default: ０
+    perPage: 2, // 1ページに表示するスライドの数 @type: { number }, default: ０
+    perMove: 1, // スライダーが移動するスライドの数 @type: { number }, default: ０
+  }).mount();
+});
